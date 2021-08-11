e8cf2b59a10b4453f337f19b8c04e481d7e39d6e
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-08-11 15:53:59 -0300

    Tested the methods GET, POST, PUT and DELETE with Postman

--- src/main/java/com/auto/showroom/api/IndexController.java
+++ src/main/java/com/auto/showroom/api/IndexController.java
@@ -1,6 +1,9 @@
 package com.auto.showroom.api;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@GetMapping
-	public String hello() {
-		return "Hello Spring Boot 2!";
+	public String get() {
+		return "GET Spring Boot 2!";
+	}
+	
+	@PostMapping
+	public String post() {
+		return "POST Spring Boot 2!";
+	}
+	
+	@PutMapping
+	public String put() {
+		return "PUT Spring Boot 2!";
+	}
+	
+	@DeleteMapping
+	public String delete() {
+		return "DELETE Spring Boot 2!";
 	}
 	
 }
