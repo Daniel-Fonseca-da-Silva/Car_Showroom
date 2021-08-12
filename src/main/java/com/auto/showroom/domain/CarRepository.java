64c9f282d005035bf898a22b2b2c38bea936604f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-08-12 19:16:10 -0300

    Created new method in srepository to search car using category

--- src/main/java/com/auto/showroom/domain/CarRepository.java
+++ src/main/java/com/auto/showroom/domain/CarRepository.java
@@ -3,6 +3,8 @@ package com.auto.showroom.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarRepository extends CrudRepository<Car, Long>{
+
+	Iterable<Car> findByCategory(String category);
 	
 	
 }
