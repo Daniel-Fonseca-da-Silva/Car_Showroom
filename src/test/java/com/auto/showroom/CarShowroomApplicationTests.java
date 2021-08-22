87594e82fcd581d07d32e367b6497cb464f5ccf7
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-08-22 15:55:41 -0300

    Created the test of insert

--- src/test/java/com/auto/showroom/CarShowroomApplicationTests.java
+++ src/test/java/com/auto/showroom/CarShowroomApplicationTests.java
@@ -1,19 +1,30 @@
 package com.auto.showroom;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.auto.showroom.domain.Car;
+import com.auto.showroom.domain.CarService;
 
 @SpringBootTest
 class CarShowroomApplicationTests {
 
+	@Autowired
+	private CarService service;
+
 	@Test
-	public void test1() {
-		
+	public void insert() {
+
+		Car car = new Car();
+		car.setName("Graciela");
+		car.setCategory("classic");
+		service.insert(car);
 	}
-	
+
 	@Test
 	public void test2() {
-		
+
 	}
 
 }
