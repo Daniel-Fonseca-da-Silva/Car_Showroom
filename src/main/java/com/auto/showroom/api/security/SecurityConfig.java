d5ce1fcb151b328b26a92f32cbfb2e5afc8dfb42
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-08-28 23:34:13 -0300

    Deactivated the CSRF security

--- src/main/java/com/auto/showroom/api/security/SecurityConfig.java
+++ src/main/java/com/auto/showroom/api/security/SecurityConfig.java
@@ -7,11 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests(requests -> requests.anyRequest().authenticated());
-		http.httpBasic();
+		http.httpBasic().and().csrf().disable();
 	}
 }
