dd9775c74354f6ee0d089546c022a9d45883a095
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-08-30 15:21:50 -0300

    Implemented the userDetails

--- src/main/java/com/auto/showroom/api/IndexController.java
+++ src/main/java/com/auto/showroom/api/IndexController.java
@@ -1,5 +1,7 @@
 package com.auto.showroom.api;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,11 @@ public class IndexController {
 	@GetMapping
 	public String get() {
 		return "API from cars";
+	}
+	
+	@GetMapping("/userInfo")
+	public UserDetails userInfo(@AuthenticationPrincipal UserDetails user) {
+		return user;
 	}
 	
 }
