99421f30a841269455021c0c0c1ddeb33c50a399
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-08-30 20:16:59 -0300

    Created the method loadUserByUsername

--- src/main/java/com/auto/showroom/api/security/SecurityConfig.java
+++ src/main/java/com/auto/showroom/api/security/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.auto.showroom.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	@Qualifier("userDetailsService")
+	private UserDetailsService userDetailsService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -21,13 +28,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		BCryptPasswordEncoder encoder  = new BCryptPasswordEncoder();
-		
-		auth
-				// enable in memory based authentication with a user named
-				// "user" and "admin"
-				.inMemoryAuthentication().passwordEncoder(encoder).withUser("user").password(encoder.encode("user")).roles("USER").and().withUser("admin")
-				.password(encoder.encode("admin")).roles("USER", "ADMIN");
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
 	}
 }
---
+++ src/main/java/com/auto/showroom/api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.auto.showroom.api.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service(value = "userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		if (username.equals("user")) {
+			return User.withUsername(username).password(encoder.encode("user")).roles("USER").build();
+		} else if (username.equals("admin")) {
+			return User.withUsername(username).password(encoder.encode("admin")).roles("USER", "ADMIN").build();
+		}
+
+		throw new UsernameNotFoundException("User not found");
+	}
+}
