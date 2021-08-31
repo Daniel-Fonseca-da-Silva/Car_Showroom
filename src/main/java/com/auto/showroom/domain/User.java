83dbb7ed51b9ab7c48206f597d29301907b8fb21
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-08-31 02:50:35 -0300

    Added the annotations for user

--- src/main/java/com/auto/showroom/domain/User.java
+++ src/main/java/com/auto/showroom/domain/User.java
@@ -1,10 +1,18 @@
 package com.auto.showroom.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
 
 @Entity
+@Data
 public class User {
 	
+	@Id // This is the PRIMARY KEY
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
 	private Long id;
 	private String name;
 	private String login;
